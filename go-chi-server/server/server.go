359db333733e36d6dd7eb5545749de844a6b9af5
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-04-29 13:07:45 +0900

    handle graceful shutdown

--- go-chi-server/server/server.go
+++ go-chi-server/server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -25,6 +27,14 @@ func New(
 }
 
 func (s *Server) Serve() {
+	// Create context that listens for the interrupt signal from the OS.
+	// We do this before everything else as
+	// We should be capable of catching signal as soon as the program starts
+	// https://henvic.dev/posts/signal-notify-context/
+	// https://millhouse.dev/posts/graceful-shutdowns-in-golang-with-signal-notify-context
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	logger := s.logger
 	port := s.getPort()
 	host := s.getHost()
@@ -37,13 +47,49 @@ func (s *Server) Serve() {
 	logger = logger.With().Str("Addr", server.Addr).Logger()
 
 	// Starting server
-	logger.Info().Msg("Listening")
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Error().Err(err).Msg("Failed to listen and serve")
-	} else {
-		logger.Info().Msg("Server shutdown successfully")
+	// The server is started on a separate goroutine
+	// ListenAndServe is a blocking function,
+	// It fields all incoming requests on separate goroutine
+	// https://medium.com/honestbee-tw-engineer/gracefully-shutdown-in-go-http-server-5f5e6b83da5a
+	go func() {
+		logger.Info().Msg("Listening")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			// Error starting or closing listener
+			logger.Error().Err(err).Msg("Failed to listen and serve")
+		} else {
+			// This block is only executed when the blocking function
+			// ListenAndServe stops and the server is close (ErrServerClosed)
+			logger.Info().Msg("Server shutdown successfully")
+		}
+	}()
+
+	// Block main goroutine to wait for receiving interrupt signal
+	<-ctx.Done()
+
+	// Received interrupt signal. Proceed to graceful shutdown
+	// stop()
+	logger.Info().Msg("Shutting down gracefully")
+
+	// Timeout for graceful shutdown
+	// Why need a timeout context?
+	// srv.Shutdown does not interrupt active coonections.
+	// It works by first closing all open listeners, then closing all idle connections,
+	// and then **waiting indefinitely** for active connections to return to idle and then shut down.
+	// If the provided context expires before the shutdown is complete,
+	// Shutdown returns the context’s error,
+	// otherwise it returns any error returned from closing the Server’s underlying Listener(s).
+	shutdownTimeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// shutdown
+	if err := server.Shutdown(shutdownTimeoutCtx); err != nil {
+		logger.Fatal().Err(err).Msg("Server shutdown failed")
 	}
-	// Handle interrupt signals
+	logger.Info().Msg("Server exited properly")
+}
+
+func (s *Server) Shutdown() {
+
 }
 
 func (s *Server) getPort() string {
