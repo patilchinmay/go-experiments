4b76d97f29814d686b25388017cc83c28124e5f0
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-04-30 21:58:13 +0900

    move interrupt handling to main

--- go-chi-server/main.go
+++ go-chi-server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/go-chi/httplog"
 	"github.com/joho/godotenv"
@@ -12,6 +14,14 @@ import (
 func main() {
 	// If we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Create context that listens for the interrupt signal from the OS.
+	// We do this at the beginning of the program as
+	// We should be capable of catching signal as soon as the program starts
+	// https://henvic.dev/posts/signal-notify-context/
+	// https://millhouse.dev/posts/graceful-shutdowns-in-golang-with-signal-notify-context
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
 	// Create logger
 	logger := httplog.NewLogger("My App")
@@ -47,6 +57,18 @@ func main() {
 		},
 	})
 
-	// Start server
-	server.New(logger).Serve()
+	// Create server
+	server := server.New(logger)
+	// The server is started on a separate goroutine as
+	// ListenAndServe is a blocking function,
+	// It fields all incoming requests on separate goroutine
+	// https://medium.com/honestbee-tw-engineer/gracefully-shutdown-in-go-http-server-5f5e6b83da5a
+	go server.Serve()
+
+	// Block main goroutine to wait for receiving os interrupt signal
+	<-ctx.Done()
+
+	// Received interrupt signal. Proceed to graceful shutdown
+	logger.Info().Msg("Received interrupt, shutting down gracefully")
+	server.Shutdown()
 }
