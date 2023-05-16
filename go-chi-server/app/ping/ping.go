ecc4ad1f50c61490611f97d6c6a8b7614e9ad357
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-05-16 13:34:26 +0530

    ping should respond with requestID

--- go-chi-server/app/ping/ping.go
+++ go-chi-server/app/ping/ping.go
@@ -1,9 +1,11 @@
 package ping
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httplog"
 	"github.com/patilchinmay/go-experiments/go-chi-server/app"
 )
@@ -50,11 +52,16 @@ func (p *Ping) Ping(w http.ResponseWriter, r *http.Request) {
 	oplog := httplog.LogEntry(r.Context())
 	oplog.Debug().Msg("Pong")
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Key", "Value")
-	w.WriteHeader(http.StatusOK)
+	requestID := middleware.GetReqID(r.Context())
+	oplog.Debug().Str("requestID", requestID).Msg("")
 
-	w.Write([]byte(`{"Ping":"Pong"}`))
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("requestID", requestID)
+
+	resp := fmt.Sprintf(`{"Ping":"Pong","requestID":%s}`, requestID)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(resp))
 }
 
 // init initializes the ping subrouter and
