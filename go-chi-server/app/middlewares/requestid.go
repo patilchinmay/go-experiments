5ee36a46fa23152e6f485fd57f0c23faf03ba629
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-05-21 18:28:40 +0530

    added comments

--- go-chi-server/app/middlewares/requestid.go
+++ go-chi-server/app/middlewares/requestid.go
@@ -11,10 +11,13 @@ func RequestID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// oplog := httplog.LogEntry(r.Context())
 
-		requestID := middleware.GetReqID(r.Context())
-		// oplog.Debug().Str("requestID", requestID).Msg("")
+		// oplog.Debug().Msg("Executed before request is passed to handler")
 
+		requestID := middleware.GetReqID(r.Context())
 		w.Header().Set("Request-ID", requestID)
+
 		next.ServeHTTP(w, r)
+
+		// oplog.Debug().Msg("Executed after request is passed to the handler")
 	})
 }
