56c92289352f6dc26f7159f9f8e3731d6211ca4e
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-05-29 18:19:50 +0530

    add model

--- go-chi-server/app/users/models.go
+++ go-chi-server/app/users/models.go
@@ -1,1 +1,12 @@
 package user
+
+import "gorm.io/gorm"
+
+// User
+type User struct {
+	gorm.Model
+	FirstName string `json:"firstname" validate:"required" gorm:"primaryKey"`
+	LastName  string `json:"lastname" validate:"required"`
+	Age       uint8  `json:"age" validate:"gte=0,lte=130"`
+	Email     string `json:"email" validate:"required,email"`
+}
