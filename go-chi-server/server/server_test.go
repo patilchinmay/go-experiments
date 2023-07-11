bdcf5f4a5b463af7d7ebc9794409e9f911cca181
Chinmay Patil <chinmay.patil@woven-planet.global>
2023-07-11 20:01:34 +0900

    address intermittent test failure

--- go-chi-server/server/server_test.go
+++ go-chi-server/server/server_test.go
@@ -15,13 +15,18 @@ import (
 var _ = Describe("Server", Serial, func() {
 	var ts = &server.Server{}
 	host := "127.0.0.1"
-	var portInt, _ = freeport.GetFreePort()
-	var port = strconv.Itoa(portInt)
+	var port string
 
 	BeforeEach(func() {
+		portInt, _ := freeport.GetFreePort()
+		port = strconv.Itoa(portInt)
+
 		// Create server
 		ts = server.New().WithLogger(zerolog.Nop()).WithHost(host).WithPort(port)
 		go ts.Serve()
+
+		// Allowing the goroutine to start. This is not good practice. Need to change.
+		time.Sleep(1 * time.Second)
 	})
 
 	AfterEach(func() {
