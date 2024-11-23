dfaa8331d29a86dad79620f098c74ad950364e8c
Chinmay Patil <patilchinmay01@gmail.com>
2024-11-23 15:50:50 +0900

    update main

--- GOTTH-stack-frontend/cmd/server/main.go
+++ GOTTH-stack-frontend/cmd/server/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/lmittmann/tint"
+
+	"github.com/patilchinmay/go-experiments/gotth-stack-frontend/internal/handlers"
+)
+
+func main() {
+	logger := slog.New(tint.NewHandler(os.Stdout, &tint.Options{Level: slog.LevelDebug, AddSource: true}))
+
+	e := echo.New()
+
+	// Middlewares
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
+	e.Use(middleware.Recover())
+
+	// Static files
+	e.Static("/public", "public")
+
+	// Routes
+	e.GET("/", handlers.Home)
+
+	// Start server
+	e.Logger.Fatal(e.Start("127.0.0.1:3000"))
+}
